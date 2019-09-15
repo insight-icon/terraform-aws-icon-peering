eedfbe79b34a06acff519538462147cd21dd532e
robc-io <robc.io.opensource@gmail.com>
2019-09-14 20:31:18 -0700

    fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "connection_id" {
-  value       = join("", aws_vpc_peering_connection.default.*.id)
+  value       = aws_vpc_peering_connection.this.id
   description = "VPC peering connection ID"
 }
 
 output "accept_status" {
-  value       = join("", aws_vpc_peering_connection.default.*.accept_status)
+  value       = aws_vpc_peering_connection.this.accept_status
   description = "The status of the VPC peering connection request"
 }
 
